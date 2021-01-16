2a15783db027bc41ad97164f5ae44dab589eee39
Rizky Fakkel <rizky_fakkel@hotmail.com>
2021-01-16 20:48:17 +0100

    More product features!

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,6 +1,36 @@
 <template>
   <q-page class="column items-top justify-start q-px-xl q-py-md">
     <span class="text-h1 text-white">Products</span>
+    <q-slide-transition appear>
+      <div v-show="selected.length">
+        <q-btn
+          @click="confirmShowSelected"
+          icon="visibility"
+          key="positive"
+          class="q-ma-sm inline-block"
+          rounded
+          color="positive"
+          size="md"
+          label="Show"/>
+        <q-btn
+          @click="confirmHideSelected"
+          icon="visibility_off"
+          key="warning"
+          class="q-ma-sm inline-block"
+          rounded
+          color="info"
+          size="md"
+          label="Hide"/>
+        <q-btn
+          @click="confirmRemoveSelected"
+          icon="delete"
+          class="q-ma-sm inline-block"
+          rounded
+          color="negative"
+          size="md"
+          label="Remove"/>
+      </div>
+    </q-slide-transition>
     <div class="bg-accent rounded-borders">
       <q-dialog v-model="productFormState" persistent transition-show="scale" transition-hide="scale">
         <q-card :class="{ 'bg-dark': $q.dark.isActive, 'bg-primary': !$q.dark.isActive }" class="text-white"
@@ -341,12 +371,12 @@ export default class Products extends Vue {
   }
 
   protected onRowClick(event: any, row: any) {
-    this.$router.push({name: 'product', params: { id: row.id}});
+    this.$router.push({name: 'product', params: {id: row.id}});
   }
 
   protected objectFlip(obj: any) {
     const ret: {
-      [key: string] : any;
+      [key: string]: any;
     } = {};
     Object.keys(obj).forEach(key => {
       ret[obj[key]] = key;
@@ -406,6 +436,102 @@ export default class Products extends Vue {
         this.fetchProducts();
       }
     );
+  }
+
+  confirmShowSelected () {
+    const selectedIds = this.selected.map(
+      (selectedItem: {id: number}) => {
+        return selectedItem.id;
+      }
+    );
+    this.$q.dialog({
+      title: 'Confirm',
+      message: `Would you like to show all ${selectedIds.length} selected products?`,
+      cancel: true,
+    }).onOk(() => {
+      this.$q.loading.show();
+      this.$axios.post('/products/unhide.multiple', selectedIds).then(
+        () => {
+          this.fetchProducts();
+          this.$q.notify({
+            message: 'Done!',
+            type: 'positive',
+          });
+        }
+      ).finally(
+        () => {
+          this.$q.loading.hide();
+        }
+      )
+    }).onCancel(() => {
+      // console.log('>>>> Cancel')
+    }).onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
+    });
+  }
+
+  confirmHideSelected () {
+    const selectedIds = this.selected.map(
+      (selectedItem: {id: number}) => {
+        return selectedItem.id;
+      }
+    );
+    this.$q.dialog({
+      title: 'Confirm',
+      message: `Would you like to hide all ${selectedIds.length} selected products?`,
+      cancel: true,
+    }).onOk(() => {
+      this.$q.loading.show();
+      this.$axios.post('/products/hide.multiple', selectedIds).then(
+        () => {
+          this.fetchProducts();
+          this.$q.notify({
+            message: 'Done!',
+            type: 'positive',
+          });
+        }
+      ).finally(
+        () => {
+          this.$q.loading.hide();
+        }
+      )
+    }).onCancel(() => {
+      // console.log('>>>> Cancel')
+    }).onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
+    });
+  }
+
+  confirmRemoveSelected () {
+    const selectedIds = this.selected.map(
+      (selectedItem: {id: number}) => {
+        return selectedItem.id;
+      }
+    );
+    this.$q.dialog({
+      title: 'Confirm',
+      message: `Would you like to remove all ${selectedIds.length} selected products?`,
+      cancel: true,
+    }).onOk(() => {
+      this.$q.loading.show();
+      this.$axios.post('/products/remove.multiple', selectedIds).then(
+        () => {
+          this.fetchProducts();
+          this.$q.notify({
+            message: 'Done!',
+            type: 'positive',
+          });
+        }
+      ).finally(
+        () => {
+          this.$q.loading.hide();
+        }
+      )
+    }).onCancel(() => {
+
+    }).onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
+    });
   }
 };
 </script>
